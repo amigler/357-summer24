0ecdadda825d332692ee410a0ff9dd80e4612e81
amigler <amigler@calpoly.edu>
2024-06-27 00:08:58 +0000

    added comments

--- week1/getc.c
+++ week1/getc.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+// read characters one-at-a-time from stdin, print each one out
 int main(int argc, char* argv[]) {
 
-    // declaration automatically provided in stdio.h
+    // this declaration is automatically provided in stdio.h
     // FILE *stdin;
     
     int c = getc(stdin);
