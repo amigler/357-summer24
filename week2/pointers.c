2386116d26d7c0374e7f8f726b91133b8098044a
amigler <amigler@calpoly.edu>
2024-07-01 23:01:44 +0000

    2024-07-01

--- week2/pointers.c
+++ week2/pointers.c
@@ -3,9 +3,13 @@
 
 int main(int argc, char* argv[]) {
     char vowels[] = { 'a', 'e', 'i', 'o', 'u' };
+    char next_str[] = "these are not vowels";
 
     char *v = &vowels[0];
+    char *v_str = vowels;
 
     printf("Pointer: %p, value: %c\n", v, *v);
-    printf("Pointer: %p, value: %c\n", v+1, *(v+1));    
+    printf("Pointer: %p, value: %c\n", v+1, *(v+1));
+
+    printf("As a string: %s\n", v_str); 
 }
