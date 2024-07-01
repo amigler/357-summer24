9c76287d482c2e27e5f8886e29a7d04f220c5539
amigler <amigler@calpoly.edu>
2024-07-01 23:10:10 +0000

    bsec functions

---
+++ week2/bseq_fns.c
@@ -0,0 +1,11 @@
+#include "bseq.h"
+
+void read_ints(FILE *fp) {
+    int oc = 0, i = 0;
+    while ((oc = fread(&i, sizeof(int), 1, fp)) == 1) {
+        printf("%d\n", i);
+    }
+
+    //char c = ' ';
+    //fread(&c, sizeof(char), 32, fp);  // read 32 bytes/chars into the variable c
+}
