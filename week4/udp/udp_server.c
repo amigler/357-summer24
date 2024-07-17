70224dca8351c808e4c36c204a4d10a3c1f54ee8
amigler <amigler@calpoly.edu>
2024-07-17 22:09:04 +0000

    udp_server

--- week4/udp/udp_server.c
+++ week4/udp/udp_server.c
@@ -1,44 +1,44 @@
 #include <arpa/inet.h>
 #include <getopt.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define MESG "Hello from client\n"
+#define MESG "Message from server\n"
 #define MAXLEN 1000
 
 int main(int argc, char *argv[]) {
     int sock_fd;
     struct sockaddr_in sa;
-    struct hostent *hostent;
-    char buff[MAXLEN+1];
-    int len, mlen;
-    socklen_t slen;
-    int port = 10000;
-    char *hostname = "localhost";
+    socklen_t len;
     
-    hostent = gethostbyname(hostname);
+    char localaddr[INET_ADDRSTRLEN];
+    char buff[MAXLEN+1];
+    int port = 10000;
+    
+    socklen_t slen;
     
     sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
     
     sa.sin_family = AF_INET;
-    sa.sin_port = htons(port);
-    sa.sin_addr.s_addr = *(uint32_t*) hostent->h_addr_list[0];
-    mlen = strlen(MESG);
-    len = sendto(sock_fd, MESG, mlen, 0, (struct sockaddr *) &sa, sizeof(sa));
+    sa.sin_port = htons(port); // use designated port
     
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(port);
-    sa.sin_addr.s_addr = htonl(INADDR_ANY);
+    sa.sin_addr.s_addr = htonl(INADDR_ANY); // any local interfaces
+    bind(sock_fd, (struct sockaddr *) &sa, sizeof(sa));  
+    
     slen = sizeof(sa);
+    int n = recvfrom(sock_fd, buff, sizeof(buff),0, (struct sockaddr *) &sa, &slen);
+
+    inet_ntop(AF_INET, &sa.sin_addr.s_addr, localaddr, sizeof(localaddr));
     
-    len = recvfrom(sock_fd, buff, sizeof(buff), 0, (struct sockaddr *) &sa, &slen);
-    write(STDOUT_FILENO, buff, len);
+    printf("Message from <%s:%d>: ", localaddr, ntohs(sa.sin_port));
+    fflush(stdout);
+    
+    write(STDOUT_FILENO, buff, strlen(MESG));
+    len = sendto(sock_fd, MESG, strlen(MESG), 0, (struct sockaddr *) &sa, sizeof(sa));
     
     shutdown(sock_fd, SHUT_RDWR);
     
