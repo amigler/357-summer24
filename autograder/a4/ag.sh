28fb87266c1d2db2a1704fe7e52f3ff7ec48f9a2
amigler <amigler@calpoly.edu>
2024-07-26 23:19:19 +0000

    a4 ag message fix

--- autograder/a4/ag.sh
+++ autograder/a4/ag.sh
@@ -207,7 +207,7 @@ elif [ "$1" = "cgi" ]; then
 
     ((total++))
     echo ""
-    echo "Test Case #$total: GET /cgi-bin/usr/bin/wc?autograder%20word%20count%0Atest HTTP/1.1"
+    echo "Test Case #$total: GET /cgi-bin/usr/bin/wc?autograder%20word%20count%0Atest%0A HTTP/1.1"
     #timeout 2 curl -sG http://localhost:9009/cgi-bin/wc --data-urlencode "autograder word count
     #test" 2>&1 > ag_CGI_out
 
